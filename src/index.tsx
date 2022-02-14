1b21dd23ec2d8148f4a0a2e5ecf0d8f99de27cbe
Darkhan <darkhanahmetov228@gmail.com>
2022-02-14 15:39:52 +0600

    feat and refact: added styling and change component folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import * as esbuild from 'esbuild-wasm'
