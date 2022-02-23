5410de2c766e4b911cc2ce76708b28f59fba2448
Darkhan <darkhanahmetov228@gmail.com>
2022-02-23 20:37:44 +0600

    refact: extracted bundling logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import * as esbuild from 'esbuild-wasm'
-import { unpkgPathPlugin, fetchPlugin } from './plugins'
 import { CodeEditor, Preview } from './components'
+import bundle from './bundler'
 
 const App = () => {
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
-  const serviceRef = useRef<esbuild.Service>()
-
-  const startService = async () => {
-    serviceRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    })
-  }
-
   const onClickHandler = async () => {
-    if (!serviceRef.current) {
-      return
-    }
+    const output = await bundle(input)
 
-    const result = await serviceRef.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    })
-
-    setCode(result.outputFiles[0].text)
+    setCode(output)
   }
-
-  useEffect(() => {
-    startService()
-  }, [])
 
   return (
     <div>
